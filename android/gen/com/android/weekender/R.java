68a96bedfe67dda187f55db0608a2ab985af78d3
Arshi <arshi@Arshis-MacBook-Air.local>
2014-06-13 17:33:58 -0400

    Added login UI

--- android/gen/com/android/weekender/R.java
+++ android/gen/com/android/weekender/R.java
@@ -995,10 +995,11 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050042;
+        public static final int action_settings=0x7f050043;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int button1=0x7f050042;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
@@ -1006,8 +1007,8 @@ containing a value of this type.
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
-        public static final int editText1=0x7f05003f;
-        public static final int editText2=0x7f05003e;
+        public static final int editText1=0x7f050040;
+        public static final int editText2=0x7f05003f;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
@@ -1044,8 +1045,8 @@ containing a value of this type.
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
         public static final int textView1=0x7f05003d;
-        public static final int textView2=0x7f050040;
-        public static final int textView3=0x7f050041;
+        public static final int textView2=0x7f050041;
+        public static final int textView3=0x7f05003e;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1152,6 +1153,7 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
+        public static final int login_button=0x7f0a0012;
         public static final int password=0x7f0a0011;
         public static final int username=0x7f0a0010;
     }
