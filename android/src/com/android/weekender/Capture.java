0c97c4e8caccdbf0469f44571778471f499fd96b
vedant khattar <vkhattar.uwaterloo.ca>
2014-06-18 23:49:13 -0400

    added camera button on login screen for now for testing purposes

---
+++ android/src/com/android/weekender/Capture.java
@@ -0,0 +1,49 @@
+package com.android.weekender;
+
+
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.os.Build;
+
+public class Capture extends ActionBarActivity {
+
+	  private static final int CAMERA_REQUEST = 1888; 
+	    private ImageView imageView;
+
+	    @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.cam);
+	        imageView = (ImageView)findViewById(R.id.imageView1);
+	        Button photoButton = (Button)findViewById(R.id.button1);
+	        photoButton.setOnClickListener(new View.OnClickListener() {
+
+	            @Override
+	            public void onClick(View v) {
+	                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE); 
+	                startActivityForResult(cameraIntent, CAMERA_REQUEST); 
+	            }
+	        });
+	    }
+
+	    protected void onActivityResult(int requestCode, int resultCode, Intent data) {  
+	        if (requestCode == CAMERA_REQUEST) {  
+	            Bitmap photo = (Bitmap) data.getExtras().get("data"); 
+	            imageView.setImageBitmap(photo);
+	        }  
+
+	    }
+}
--- android/src/com/android/weekender/Login.java
+++ android/src/com/android/weekender/Login.java
@@ -1,28 +1,47 @@
 package com.android.weekender;
 
+
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import android.os.Build;
 
 public class Login extends ActionBarActivity {
 
-    @Override
+	
+	static final String EXTRA_MESSAGE = "com.android.weekender.MESSAGE";
+
+	// Called when the user clicks the Send button
+		public void capturePic(View view){
+			//Do something in response to the button
+			Intent intent = new Intent(this, Capture.class);
+			String message = "hello";
+			intent.putExtra(EXTRA_MESSAGE, message);
+			startActivity(intent);
+		}
+	
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+       
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+        
     }
 
 
