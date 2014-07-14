0399d6542baf7cf0d3752d1ce1b679b46c02f597
ksdnlee <c.j.lee.h.y@gmail.com>
2014-07-14 02:51:14 -0400

    Updated app icon and logic for timer

--- android/src/com/android/weekender/TimerActivity.java
+++ android/src/com/android/weekender/TimerActivity.java
@@ -1,6 +1,14 @@
 package com.android.weekender;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import com.parse.FunctionCallback;
+import com.parse.ParseCloud;
+import com.parse.ParseException;
+import com.parse.ParseObject;
 
 import android.os.Bundle;
 
@@ -9,60 +17,57 @@ import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
 
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TimerActivity extends ActionBarActivity {
 
 	TextView timeView;
+	TextView message;
 
-	private static final String FORMAT = "%02d:%02d:%02d";
+	private static final String FORMAT = "%02d:%02d:%02d:%02d";
 
 	int seconds, minutes;
-
+	String timeLeftInMiliseconds, isWeekend;
+	
 	public void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_timer);
-
+		
 		timeView = (TextView) findViewById(R.id.timeView);
+		message = (TextView) findViewById(R.id.message);
+		
+		//get time from parse
+		HashMap<String, Object> params = new HashMap<String, Object>();
+		try {
+			timeLeftInMiliseconds = ParseCloud.callFunction("getTimeLeft", params).toString();
+			isWeekend = ParseCloud.callFunction("isWeekend", params).toString();
 
-		String timeInMilliseconds = "1405058785773";
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-		String millisecondsInADay = "86400000";
-
-		new CountDownTimer(Long.valueOf(timeInMilliseconds).longValue(), 1000) {
+		if (isWeekend.equals("true")) {
+			message.setText("TIME LEFT TO POST YOUR FUN");
+		} else {
+			message.setText("TIME BEFORE PARTY TIME");
+		}
+		
+		new CountDownTimer(Long.valueOf(timeLeftInMiliseconds).longValue(), 1000) {
 
 			public void onTick(long millisUntilFinished) {
+				
+				timeView.setText(""	+ String.format(FORMAT,
+				
+				TimeUnit.MILLISECONDS.toDays(millisUntilFinished),
+				
+				TimeUnit.MILLISECONDS.toHours(millisUntilFinished) - TimeUnit.DAYS.toHours(TimeUnit.MILLISECONDS.toDays(millisUntilFinished)),
 
-				timeView.setText(""
+				TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millisUntilFinished)),
 
-				+ String.format(
-
-				FORMAT,
-
-				TimeUnit.MILLISECONDS
-
-				.toHours(millisUntilFinished),
-
-				TimeUnit.MILLISECONDS
-
-				.toMinutes(millisUntilFinished)
-
-				- TimeUnit.HOURS
-
-				.toMinutes(TimeUnit.MILLISECONDS
-
-				.toHours(millisUntilFinished)),
-
-				TimeUnit.MILLISECONDS
-
-				.toSeconds(millisUntilFinished)
-
-				- TimeUnit.MINUTES
-
-				.toSeconds(TimeUnit.MILLISECONDS
-
-				.toMinutes(millisUntilFinished))));
+				TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
 
 			}
 
@@ -76,4 +81,5 @@ public class TimerActivity extends ActionBarActivity {
 
 	}
 
+	
 }
