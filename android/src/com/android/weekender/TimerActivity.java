e3991355cedf6406977a65c627280d409313e204
ProLegend <wilson@v1020-wn-12-233.campus-dynamic.uwaterloo.ca>
2014-07-13 22:28:42 -0400

    Timer code added

--- android/src/com/android/weekender/TimerActivity.java
+++ android/src/com/android/weekender/TimerActivity.java
@@ -1,5 +1,79 @@
 package com.android.weekender;
 
-public class TimerActivity {
+import java.util.concurrent.TimeUnit;
+
+import android.os.Bundle;
+
+import android.os.CountDownTimer;
+
+import android.support.v7.app.ActionBarActivity;
+
+import android.widget.TextView;
+
+public class TimerActivity extends ActionBarActivity {
+
+	TextView timeView;
+
+	private static final String FORMAT = "%02d:%02d:%02d";
+
+	int seconds, minutes;
+
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_timer);
+
+		timeView = (TextView) findViewById(R.id.timeView);
+
+		String timeInMilliseconds = "1405058785773";
+
+		String millisecondsInADay = "86400000";
+
+		new CountDownTimer(Long.valueOf(timeInMilliseconds).longValue(), 1000) {
+
+			public void onTick(long millisUntilFinished) {
+
+				timeView.setText(""
+
+				+ String.format(
+
+				FORMAT,
+
+				TimeUnit.MILLISECONDS
+
+				.toHours(millisUntilFinished),
+
+				TimeUnit.MILLISECONDS
+
+				.toMinutes(millisUntilFinished)
+
+				- TimeUnit.HOURS
+
+				.toMinutes(TimeUnit.MILLISECONDS
+
+				.toHours(millisUntilFinished)),
+
+				TimeUnit.MILLISECONDS
+
+				.toSeconds(millisUntilFinished)
+
+				- TimeUnit.MINUTES
+
+				.toSeconds(TimeUnit.MILLISECONDS
+
+				.toMinutes(millisUntilFinished))));
+
+			}
+
+			public void onFinish() {
+
+				timeView.setText("done!");
+
+			}
+
+		}.start();
+
+	}
 
 }
